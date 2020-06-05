4faf692b870c1a6165725da734de5f1a92cf23a1
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-06-05 16:45:05 +0100

    Add base structure for the project
    Based on what was discussed, all classes needed have been added.

--- Roguelike/Program.cs
+++ Roguelike/Program.cs
@@ -6,7 +6,23 @@ namespace Roguelike
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while(true)
+            {
+                Console.SetCursorPosition(0, 0);
+
+                Console.Write("Rendering test!\n");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for(int j = 0; j < 10; j++)
+                    {
+                        Console.Write(".");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey(true);
+            }
         }
     }
 }
