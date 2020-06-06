29d3485baff35d88d0107c4bf10af5f1923956ea
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-06-06 21:34:35 +0100

    Add AbstractMovement
    Following the reference of the teacher's project for good measure.

---
+++ Roguelike/AbstractMovement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roguelike
+{
+    public abstract class AbstractMovement
+    {
+        protected readonly IReadOnlyWorld world;
+
+        protected AbstractMovement(IReadOnlyWorld world)
+        {
+            this.world = world;
+        }
+
+        public abstract Position WhereToMove(Agent agent);
+    }
+}
