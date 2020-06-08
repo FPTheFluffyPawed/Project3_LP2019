09c9efc70ff85afb77938a4f90f32d2c13daacf4
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-06-08 17:28:04 +0100

    Add base (random) enemy movement to test
    For now, enemies move randomly just so I could test their collissions with the player.

--- Roguelike/EnemyMovement.cs
+++ Roguelike/EnemyMovement.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Roguelike
 {
-    class EnemyMovement
+    public class EnemyMovement : AbstractMovement
     {
+        public EnemyMovement(IReadOnlyWorld world) : base(world)
+        {
+
+        }
+
+        public override Position WhereToMove(Agent agent)
+        {
+            Thread.Sleep(500);
+
+            Random random = new Random();
+
+            Direction direction;
+
+            switch (random.Next(3))
+            {
+                case 0:
+                    direction = Direction.Up;
+                    break;
+                case 1:
+                    direction = Direction.Down;
+                    break;
+                case 2:
+                    direction = Direction.Left;
+                    break;
+                default:
+                    direction = Direction.Right;
+                    break;
+            }
+
+            return world.GetNeighbor(agent.Pos, direction);
+        }
     }
 }
