ca07063fc52d5f005e6ec47ac53f8d7b25b12cce
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-06-10 22:52:12 +0100

    Add Vector calculations
    Based on the reference, added the methods that calculate the vectors between two points.

--- Roguelike/World.cs
+++ Roguelike/World.cs
@@ -60,7 +60,12 @@ namespace Roguelike
 
         public Position VectorBetween(Position pos1, Position pos2)
         {
-            throw new NotImplementedException();
+            int x, y;
+
+            x = pos2.X - pos1.X;
+            y = pos2.Y - pos1.Y;
+
+            return new Position(x, y);
         }
 
         public Position GetNeighbor(Position pos, Direction dir)
@@ -90,7 +95,75 @@ namespace Roguelike
 
         public Position GetNeighbor(Position pos, Position directionVector)
         {
-            throw new NotImplementedException();
+            // Create a random to choose either Vertical or Horizontal.
+            Random random = new Random();
+
+            // Variable to contain the direction.
+            Direction direction = default(Direction);
+
+            // Convert values of the vector to 1, -1 or 0.
+            directionVector = new Position(Math.Sign(directionVector.X), Math.Sign(directionVector.Y));
+
+            if (directionVector.X == 1 && directionVector.Y == 1)
+            { 
+                switch (random.Next(2))
+                {
+                    case 0:
+                        direction = Direction.Down;
+                        break;
+                    case 1:
+                        direction = Direction.Right;
+                        break;
+                }
+            }
+            else if (directionVector.X == 1 && directionVector.Y == 0)
+                direction = Direction.Down;
+            else if (directionVector.X == 1 && directionVector.Y == -1)
+            {
+                switch (random.Next(2))
+                {
+                    case 0:
+                        direction = Direction.Down;
+                        break;
+                    case 1:
+                        direction = Direction.Left;
+                        break;
+                }
+            }
+            else if (directionVector.X == 0 && directionVector.Y == 1)
+                direction = Direction.Right;
+            else if (directionVector.X == 0 && directionVector.Y == 0)
+                direction = Direction.None;
+            else if (directionVector.X == 0 && directionVector.Y == -1)
+                direction = Direction.Left;
+            else if (directionVector.X == -1 && directionVector.Y == 1)
+            {
+                switch (random.Next(2))
+                {
+                    case 0:
+                        direction = Direction.Up;
+                        break;
+                    case 1:
+                        direction = Direction.Right;
+                        break;
+                }
+            }
+            else if (directionVector.X == -1 && directionVector.Y == 0)
+                direction = Direction.Up;
+            else if (directionVector.X == -1 && directionVector.Y == -1)
+            {
+                switch (random.Next(2))
+                {
+                    case 0:
+                        direction = Direction.Up;
+                        break;
+                    case 1:
+                        direction = Direction.Left;
+                        break;
+                }
+            }
+
+            return GetNeighbor(pos, direction);
         }
     }
 }
