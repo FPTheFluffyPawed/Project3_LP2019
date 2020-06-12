f54bfcca6d6564046012ba8d3e2d54aaf71b1c98
diogo-h <diogo.m.henriques98@gmail.com>
2020-06-12 19:45:07 +0100

    Add XML comments

--- Roguelike/Program.cs
+++ Roguelike/Program.cs
@@ -4,13 +4,21 @@ using System.Text;
 
 namespace Roguelike
 {
+    /// <summary>
+    /// Main class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main method that verifies the arguments that the player chose and
+        /// starts the game based on that.
+        /// </summary>
+        /// <param name="args"> Arguments inserted by the user </param>
         static void Main(string[] args)
         {
             int row = 0, column = 0;
 
-            (row, column) = Ola(args, row, column);
+            (row, column) = Options(args, row, column);
 
             if (row <= 0 || column <= 0)
             {
@@ -23,7 +31,14 @@ namespace Roguelike
             game.Start();
         }
 
-        private static (int, int) Ola(string[] args, int row, int column)
+        /// <summary>
+        /// Conditions for the options that the user must insert.
+        /// </summary>
+        /// <param name="args"> Arguments inserted by the user </param>
+        /// <param name="row"> Rows of the board </param>
+        /// <param name="column"> Columns of the board </param>
+        /// <returns></returns>
+        private static (int, int) Options(string[] args, int row, int column)
         {
             for (int i = 0; i < args.Length; i++)
             {
@@ -32,7 +47,7 @@ namespace Roguelike
                 if (args[i] == "-c")
                     Int32.TryParse(args[i + 1], out column);
             }
-                return (row, column);
+            return (row, column);
         }
     }
 }
